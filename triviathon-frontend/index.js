533fc5b0f68a39a72d6342075bf85eb73710a44c
Valeria <valeria.gutsuliak@gmail.com>
2019-12-03 11:07:57 -0600

    commit

--- triviathon-frontend/index.js
+++ triviathon-frontend/index.js
@@ -3,7 +3,7 @@ const USERS_URL = "http://localhost:3000/users"
 document.addEventListener('DOMContentLoaded', function(){   
    
 })
-    form.addEventListener('submit', (e) => {
+    .addEventListener('submit', (e) => {
         
 
     })
