fcc88b2d20f45bd29b3f3100fb4870acd6e8be67
Valeria <valeria.gutsuliak@gmail.com>
2019-12-03 13:25:55 -0600

    lastupdate

--- triviathon-frontend/index.js
+++ triviathon-frontend/index.js
@@ -1,12 +1,56 @@
 const USERS_URL = "http://localhost:3000/users"
+const form = document.getElementsByClassName('card')
 
 document.addEventListener('DOMContentLoaded', function(){   
+
    
+
+    form.addEventListener('submit', (e) => {
+        const postObj = {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+              'username': `${event.target.name.value}`,
+              'score' : 0
+              
+            })
+        }
+
+        function findUser(){
+        fetch(USERS_URL, postObj)
+        .then(resp => resp.json())
+        .then(users => addUsers(user))
+        }
+        form.reset()
+    
+    })
+    function getUserData(){
+        const currentUser = {
+
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                
+            
+            
+            })
+
+        }
+
+    }
+    
+    
 })
-    .addEventListener('submit', (e) => {
+
         
 
-    })
+
 
 
    
