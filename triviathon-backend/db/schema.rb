39d1c3fd2668027c39cc4432788e21b4baf0e07f
NaveedG <nganjani@protonmail.com>
2019-12-03 16:50:53 -0600

    resolved conflicts

--- triviathon-backend/db/schema.rb
+++ triviathon-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_191829) do
+ActiveRecord::Schema.define(version: 2019_12_03_164153) do
 
   create_table "answers", force: :cascade do |t|
     t.string "question"
