8b35e431f5ff5c985a73c7862eb2f49868017e53
NaveedG <nganjani@protonmail.com>
2019-12-04 14:23:20 -0600

    added welcome and start messages

--- triviathon-frontend/src/index.js
+++ triviathon-frontend/src/index.js
@@ -1,31 +1,25 @@
 const USERS_URL = "http://localhost:3000/users"
 const QUEST_URL = "https://opentdb.com/api.php?amount=5&type=multiple"
 const form = document.getElementById('login-form')
-// const butt = document.querySelector('')
 
+// main function
 function main() {
   document.addEventListener('DOMContentLoaded', function(){ 
     // render all users & scores in left sidebar
+    welcomeMessage()
     getAllUsers()
     
     form.addEventListener('submit', (e) => {
       e.preventDefault()
-      
       let user = grabUserData(e)
       e.target.reset()
-
       loginUser(user)
-      
     })
-
-    getQuestions()
-  
-
   })
 }
 
-  function loginUser(user) {
-    
+// user login functions
+function loginUser(user) {
     const name = user.username
     const configObj = {
       method: 'POST',
@@ -41,17 +35,20 @@ function main() {
     fetch(USERS_URL, configObj)
     .then(resp => resp.json())
     .then(user => renderUserInfo(user))
+    startMessage()
+    getQuestions()
   }
 
-  function grabUserData(e) {
+function grabUserData(e) {
     return {username: e.target.children[1].value} 
   }
 
-  function renderUserInfo(user) {
+function renderUserInfo(user) {
     const infosec = document.querySelector('.user-info')
     infosec.innerHTML = `<span data-id= ${user.id}><p>Name: ${user.username}</p><p>Score: ${user.score}</p>`
   }
 
+// user ranking functions
 function getAllUsers() {
   fetch(USERS_URL)
   .then(resp => resp.json())
@@ -69,12 +66,6 @@ function listUser(user) {
   userList.appendChild(userItem)
 }
 
-
-
-      
-
-
-        
 //         li.addEventListener('click',() => handelSelection(index))
       
 //       })
@@ -96,7 +87,7 @@ function listUser(user) {
   // })
  
 
-
+// render q&a functions
 function renderQuestion(questionObj) {
   const inner = document.querySelector('#question-slides')
   const slide = document.createElement('div')
@@ -104,7 +95,6 @@ function renderQuestion(questionObj) {
 
   const answerChoices = [...questionObj.incorrect_answers];
   questionObj.answerIndex = Math.floor(Math.random() * 3);
-        
       
   answerChoices.splice(
         questionObj.answerIndex,
@@ -114,9 +104,6 @@ function renderQuestion(questionObj) {
   const question_content = document.createElement('h3')
   question_content.innerHTML = questionObj.question
   
-
-  
-
   slide.appendChild(question_content)
   slide.insertAdjacentHTML('beforeend',
     `<div id="answer-form">
@@ -139,6 +126,22 @@ function getQuestions() {
   fetch(QUEST_URL)
   .then(resp => resp.json())
   .then(allQuestions => addQuestions(allQuestions))
+}
+
+// message functions
+function welcomeMessage() {
+  let carouselMsg = document.getElementById('carousel-msg')
+  let welcomeMsg = document.createElement('h3')
+  welcomeMsg.setAttribute('id', 'mid-header')
+  welcomeMsg.innerText = 'Welcome! Please Login to Continue'
+  carouselMsg.append(welcomeMsg)
+}
+
+function startMessage() {
+  let startMsg = document.getElementById('mid-header')
+  startMsg.innerText = `You have 30 seconds to answer each question
+
+                        GET READY...GET SET...`
 }
 
 
