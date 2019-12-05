c89bff441ff5abb3672a315d6044064bb0c343ba
NaveedG <nganjani@protonmail.com>
2019-12-05 11:58:53 -0600

    modified users controller

--- triviathon-backend/app/controllers/users_controller.rb
+++ triviathon-backend/app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
 
   def create
     user = User.find_or_create_by(username: params[:username])
-    render json: user
+    render json: user, include: :answers
   end
   
   def update
